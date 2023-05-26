0719e007dd2da7a628510a21ce38db4a3aa1b041
foosaan <fauzan4297@gmail.com>
2023-05-26 16:08:15 +0700

    menyisipkan sebuah node di awal dari double-linked list

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -23,3 +23,32 @@ public:
 	void hapus();
 	void searchData();
 };
+
+void doublelinkedlist::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student :";
+	cin >> nim;
+	cout << "\nEnter the name of the student :";
+	cin >> nm;
+
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; //step 2
+	newNode->name = nm; //step2
+
+	/*insert a node in the begining of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) { //check if data null
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START; //step 3
+		if (START != NULL)
+			START->prev = newNode; //step 4
+		newNode->prev = NULL; //step 5
+		START = newNode; //step6
+		return;
+		
+	}
+	
+}
