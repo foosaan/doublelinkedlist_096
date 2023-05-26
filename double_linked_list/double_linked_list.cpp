1df943286fc9b0633d7d185b8f64ddf69ce7d509
foosaan <fauzan4297@gmail.com>
2023-05-26 20:29:49 +0700

    memanggil semua prosedur

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -161,3 +161,46 @@ void doublelinkedlist::searchData() {
 	}
 }
 
+int main() {
+	doublelinkedlist obj;
+	while (true)
+	{
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record to the list" << endl;
+			cout << "3. view all record in the ascending order of roll numbers" << endl;
+			cout << "4. view all record in the descending order of roll numbers" << endl;
+			cout << "5. search for a record in the list" << endl;
+			cout << "6. exit" << endl;
+			cout << "\nEnter your choice (1-6) :" << endl;
+			char ch;
+			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.traverse();
+				break;
+			case '4':
+				obj.revtraverse();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+}
